fda01764459a51b42c1a5786bb195c36da6872c7
D Smith <daniels@giveyourmilk.org>
2022-02-25 17:29:03 -0700

    Started Body Type widget

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,19 +1,77 @@
 from fbs_runtime.application_context.PySide6 import ApplicationContext
 from fbs_runtime import PUBLIC_SETTINGS
 from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
 
 import sys
 
+class _bodytypechart(QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.setSizePolicy(
+            QSizePolicy.MinimumExpanding,
+            QSizePolicy.MinimumExpanding
+        )
+
+    def sizeHint(self):
+        return QSize(40,120)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+
+        brush = QBrush()
+        brush.setColor(QColor('black'))
+        brush.setStyle(Qt.SolidPattern)
+        rect = QRect(0, 0, painter.device().width(), painter.device().height())
+        painter.fillRect(rect, brush)
+
+class BodyTypeWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        grid = QGridLayout()
+        self._endo = QSlider(Qt.Horizontal)
+        self._ecto = QSlider(Qt.Horizontal)
+        self._meso = QSlider(Qt.Horizontal)
+
+        self._endolabel = QLabel("0")
+        self._ectolabel = QLabel("0")
+        self._mesolabel = QLabel("0")
+
+        self._chart = _bodytypechart()
+
+        grid.addWidget(self._chart,0,0)
+        grid.addWidget(self._endo,1,0)
+        grid.addWidget(self._endolabel,1,1)
+
+        grid.addWidget(self._ecto,2,0)
+        grid.addWidget(self._ectolabel,2,1)
+
+        grid.addWidget(self._meso,3,0)
+        grid.addWidget(self._mesolabel,3,1)
+        
+        self.setLayout(grid)
+
+    
+    
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.initUI()
 
-        button = QPushButton("Press me for a dialog!")
-        button.clicked.connect(self.about_dialog)
-        self.setCentralWidget(button)
+    def initUI(self):
+        
+        self.setWindowTitle("My App")
+        bt = BodyTypeWidget()
+        self.setCentralWidget(bt)
+
 
     def about_dialog(self, s):
         dlg = QMessageBox(self)
@@ -26,8 +84,8 @@ class MainWindow(QMainWindow):
             "</center>" \
             f"<p>Version {PUBLIC_SETTINGS['version']}<br/>" \
             "Copyright &copy; Delta Studio</p>"\
-            "<p>Program by Delta<br/>"\
-            "Concept by T&R</p>"
+            "<p>Program by <a href=\"https://twitter.com/nalisarc\"> Delta</a><br/>"\
+            "Concept by <a href=\"https://twitter.com/Mecknavorz\">T&R</a></p>"
     
         dlg.setText(msg)
         button = dlg.exec_()
